2eb07498f36f0ae226c2b8093589b59552fe725c
Montree2001 <montree.sr@rmuti.ac.th>
2024-12-13 15:48:28 +0700

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>หน้าหลัก</title>
+    <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
+    <link rel="stylesheet" href="assets/css/styles.min.css" />
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+    <?php include 'headbar.php'; ?>
+
+    <div class="container-fluid">
+        <h1>หน้าหลัก</h1>
+    </div>
+    
+
+
+
+
+
+
+
+<?php include 'script.php'; ?>
+</body>
+</html>
